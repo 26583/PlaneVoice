0efd720d0f3acd90f3d7c14ade948b5aef3623d8
Nanne <27259@ma-web.nl>
2020-03-10 19:45:23 +0100

    coin

---
+++ public/Coin.js
@@ -0,0 +1,11 @@
+class Coin{
+    constructor(x,y,img){
+        this.x = x;
+        this.y = y;
+        this.img = img;
+    }
+    /*coinSpawner() {
+        this.y = 150;
+        image(this.img, this.x -xpos*10, this.y, 25, 25);
+    }*/
+}
