fdfc3190ab7c3c0c079eb1b4409e70a0c68a521f
26583 <you@example.com26583@ma-web.nl>
2020-03-10 09:08:47 +0100

    logging

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var app = express();
 
 //setting middleware
 app.use(express.static('public')); //Serves resources from public folder
+console.log("Running on: localhost:5000");
 
 
 var server = app.listen(5000);
